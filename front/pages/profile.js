cbafe8d0d1492c7e755f57985f2fe774365ed9c2
newckdgns <ckdgns3167@gmail.com>
2020-11-01 02:10:13 +0900

    Node 프로젝트 생성 및 패키지 설치, Hello page 확인, Next를 왜 쓰는지 살짝 경험함

---
+++ front/pages/profile.js
@@ -0,0 +1,5 @@
+const Profile = () => {
+    return <AppLayout><div>내 프로필</div></AppLayout>
+};
+
+export default Profile
---
+++ front/pages/signup.js
@@ -0,0 +1,5 @@
+const Signup = () => {
+    return <AppLayout><div>회원가입</div></AppLayout>
+};
+
+export default Signup
